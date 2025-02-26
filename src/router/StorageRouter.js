92cf6c33a8c10cf1df7f9d9da1c32d97531e35a8
Phan Mai Quốc Kiệt <71601025+pKietDE@users.noreply.github.com>
2025-02-26 10:17:54 +0700

    Delete src directory

--- src/router/StorageRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const storageController = require('../controller/StorageController')
-
-
-router.get('/', storageController.getAllStorage)
-
-
-module.exports = router
