486c2587a968e7726e5cb6edcd05b0aabc4f0585
Phan Mai Quốc Kiệt <71601025+pKietDE@users.noreply.github.com>
2025-02-28 10:02:49 +0700

    Delete src directory

--- src/router/CustomerRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const customerController = require('../controller/CustomerController')
-
-router.get('/ngang/1', customerController.getAllCustomerHorizontal1);
-router.get('/ngang/2', customerController.getAllCustomerHorizontal2);
-
-module.exports= router
